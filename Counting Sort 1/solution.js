9eb80fff9cd71589b99102c8e78f4b58827177e3
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-11-11 22:16:35 +0200

    Create solution.js file (solution in JavaScript programming)

---
+++ Counting Sort 1/solution.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Find the maximum value in the array
+    let max_value = Math.max(...arr);
+
+    // Allocate memory for the count array and initialize it to 0
+    let count_arr = new Array(max_value + 1).fill(0);
+
+    // Count the number of occurrences of each element in the array
+    for (let i = 0; i < arr.length; i++) {
+        count_arr[arr[i]]++;
+    }
+
+    return count_arr;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = countingSort(arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
