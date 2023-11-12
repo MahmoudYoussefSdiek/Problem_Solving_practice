f9c79a917616d75f9f6038eca00f98801235ca1a
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-11-13 00:42:10 +0200

    Create solution.cs file (solution in C# programming)

---
+++ Sparse Arrays/solution.cs
@@ -0,0 +1,91 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+        // Create a dictionary to store the count of each string in the list
+        Dictionary<string, int> stringCounts = new Dictionary<string, int>();
+
+        // Count the number of times each string appears in the list
+        foreach (string str in strings)
+        {
+            if (stringCounts.ContainsKey(str))
+                stringCounts[str]++;
+            else
+                stringCounts[str] = 1;
+        }
+
+        // Create a list to store the count of each query string
+        List<int> queryCounts = new List<int>();
+
+        // Count the number of times each query string appears in the list
+        foreach (string query in queries)
+        {
+            if (stringCounts.ContainsKey(query))
+                queryCounts.Add(stringCounts[query]);
+            else
+                queryCounts.Add(0);
+        }
+
+        return queryCounts;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < stringsCount; i++)
+        {
+            string stringsItem = Console.ReadLine();
+            strings.Add(stringsItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(strings, queries);
+
+        textWriter.WriteLine(String.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
