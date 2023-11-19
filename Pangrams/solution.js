af0b780bc3b398080e9546be06c597319ecbb803
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-11-20 00:46:24 +0200

    Create solution.js file (solution in JavaScript programming)

---
+++ Pangrams/solution.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    s = s.toLowerCase();
+    const uniqueChars = new Set();
+
+    for (const character of s) {
+        if (character.match(/[a-z]/i))
+            uniqueChars.add(character);
+    }
+
+    if (uniqueChars.size === 26)
+        return "pangram";
+    else
+        return "not pangram";
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
