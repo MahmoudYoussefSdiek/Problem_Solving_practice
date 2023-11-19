bff47b8da4bdb2a0daa3b1d30a0523f0ad466967
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-11-20 00:43:10 +0200

    Create solution.py file (solution in Python programming)

---
+++ Pangrams/solution.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    s = s.lower()
+    unique_chars = set()
+
+    for character in s:
+        if character.isalpha():
+            unique_chars.add(character)
+
+    if len(unique_chars) == 26:
+        return "pangram"
+    else:
+        return "not pangram"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
