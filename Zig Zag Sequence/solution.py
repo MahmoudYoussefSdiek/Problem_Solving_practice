4fc1175fbfde5874d0472ab1013053640c1d2ddb
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-11-21 23:14:56 +0200

    Update solution.py file

--- Zig Zag Sequence/solution.py
+++ Zig Zag Sequence/solution.py
@@ -1,16 +1,42 @@
 def findZigZagSequence(a, n):
-    a.sort()
-    mid = int((n + 1)/2)
-    # a[mid], a[n-1] = a[n-1], a[mid]
-    a[mid - 1], a[n-1] = a[n-1], a[mid - 1]
+    # a.sort()
+    # mid = int((n + 1)/2)
+    # # a[mid], a[n-1] = a[n-1], a[mid]
+    # a[mid - 1], a[n-1] = a[n-1], a[mid - 1]
 
-    # st = mid + 1
-    st = mid
+    # # st = mid + 1
+    # st = mid
+    # # ed = n - 1
+    # ed = n - 2
+    # while(st <= ed):
+    #     a[st], a[ed] = a[ed], a[st]
+    #     st = st + 1
+    #     # ed = ed + 1
+    #     ed = ed - 1
+
+    # for i in range (n):
+    #     if i == n-1:
+    #         print(a[i])
+    #     else:
+    #         print(a[i], end = ' ')
+    # return
+
+    # Above code make 4 changes to make code work 
+    # but here we will change only 3 line of code
+    a.sort()
+    # Removed useless +1
+    # mid = int((n + 1)/2)
+    mid = int(n/2)
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    # -2 cause we already switched mid and end
     # ed = n - 1
     ed = n - 2
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st]
         st = st + 1
+        # -1 for decrementation
         # ed = ed + 1
         ed = ed - 1
 
